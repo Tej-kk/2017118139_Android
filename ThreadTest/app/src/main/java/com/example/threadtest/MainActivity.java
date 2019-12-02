9fa9138a2049f80d3bfc033daf9808dbe2f45cd3
Tej-kk <2017118139@stu.hstc.edu.cn>
2019-12-02 20:18:17 +0800

    点击一次按钮（创建一个进程，每个进程都有计数器），实现count自动加1

--- ThreadTest/app/src/main/java/com/example/threadtest/MainActivity.java
+++ ThreadTest/app/src/main/java/com/example/threadtest/MainActivity.java
@@ -14,7 +14,6 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private static final int UPDATE_TEXT=1;
     private TextView text;
-    private static int count=0;
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -41,27 +40,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.change_text :
-                if (count<1000){
-                    count++;
-                }
-                else{
-                    count=1;
-                }
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        Message message=new Message();
-                        message.what=UPDATE_TEXT;
-                        message.obj=new Integer(count);
-                        handler.sendMessage(message);
+                        int count=0;
+                        while(true){
+                            count++;
+                            Message message=new Message();
+                            message.what=UPDATE_TEXT;
+                            message.obj=new Integer(count);
+                            handler.sendMessage(message);
+                            try{
+                                Thread.sleep(2000);
+                            }
+                            catch (Exception e){
+
+                            }
+                        }
+
                     }
                 }).start();
-                try{
-                    Thread.sleep(1000);
-                }
-                catch (Exception e){
-
-                }
                 break;
             default:
                 break;
